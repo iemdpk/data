1a93e46fd574f17dea9021101550f6157bb4f39f
Deepak Mishra <40403460+iemdpk@users.noreply.github.com>
2022-05-06 22:04:51 +0530

    Initial commit

---
+++ src/Desktop1.js
@@ -0,0 +1,761 @@
+import React from "react";
+import styled from "styled-components";
+export const Desktop1 = ({}) => {
+  return (
+    <DesktopRoot>
+      <WhiteFlexColumn>
+        <LogoAsset src={"https://file.rendit.io/n/wt4AOZfLbcfdnX63S9QN.svg"} />
+        <Epmenu src={"https://file.rendit.io/n/ICMZth4iLFjUIgtloPmT.svg"} />
+        <Fluenthomefilled
+          src={"https://file.rendit.io/n/JOj9L21YsxunZoUIP6Kv.svg"}
+        />
+        <Fluenthomefilled
+          src={"https://file.rendit.io/n/QlgUtKplqJwXlvTuAILN.svg"}
+        />
+        <Fluenthomefilled
+          src={"https://file.rendit.io/n/44El41uigOXqKYJTHJbx.svg"}
+        />
+        <Fluenthomefilled
+          src={"https://file.rendit.io/n/MXCq04Nvwr87DaYq7tuI.svg"}
+        />
+        <Fluenthomefilled
+          src={"https://file.rendit.io/n/hV0lOGEESkMUOobC8eMP.svg"}
+        />
+        <Mdiaccount src={"https://file.rendit.io/n/p8RbDCMhMZFcHqsRH8ux.svg"} />
+        <Cilaccountlogout
+          src={"https://file.rendit.io/n/s0LXf9wNwJyGxw1omulj.svg"}
+        />
+      </WhiteFlexColumn>
+      <FlexColumn>
+        <FlexRow>
+          <NeonCarrotFlexRow>
+            <Element8>
+              <Text1>Current Value</Text1>
+              <Text2>$12</Text2>
+            </Element8>
+          </NeonCarrotFlexRow>
+          <Search src={"https://file.rendit.io/n/Gewcct4DVjLV2fD5uOBi.svg"} />
+          <Search src={"https://file.rendit.io/n/H6lMn634kSU5zWYkqTbv.svg"} />
+          <FlexRow1 alignSelf={"inherit"}>
+            <UnsplashWFsNyI1
+              src={"https://file.rendit.io/n/yvT0lE8FeL6yrsrWIfHy.png"}
+            />
+            <Text3>Julia Michaels</Text3>
+          </FlexRow1>
+        </FlexRow>
+        <FlexRow2>
+          <UnsplashWFsNyI
+            src={"https://file.rendit.io/n/WHbIJUlzY7CmPYV4FkJ9.png"}
+          />
+          <FlexColumn1>
+            <Text4>Julia Michael</Text4>
+            <FlexRow3>
+              <Room src={"https://file.rendit.io/n/ylFZvnN79WxMfg6E2Dqs.svg"} />
+              <LACAUS textTransform={"none"}>LA, CA, US </LACAUS>
+            </FlexRow3>
+          </FlexColumn1>
+          <Element9>
+            <Element10 />
+            <Text5>Edit Profile</Text5>
+          </Element9>
+          <Morehoriz
+            src={"https://file.rendit.io/n/xygcM8g4pSjbpju50zkR.svg"}
+          />
+        </FlexRow2>
+        <FlexColumn2>
+          <WhiteFlexColumn1>
+            <FlexRow4 margin={"0px 0px 12px 0px"}>
+              <Text6>About</Text6>
+              <Edit src={"https://file.rendit.io/n/I4UwwjeoZA9C7vGVGYLC.svg"} />
+            </FlexRow4>
+            <Paragraph>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Scelerisque quis vel, sed aliquam eu orci, consequat, id. dolor
+              sit amet, consectetur adipiscing elit. Scelerisque quis vel, sedt,
+              id.
+            </Paragraph>
+            <FlexRow5>
+              <Fluentballoonfilled
+                src={"https://file.rendit.io/n/QO8MALhiMeGac9KdTS58.svg"}
+              />
+              <Text7>1 Aug 2006</Text7>
+              <Fluentballoonfilled
+                src={"https://file.rendit.io/n/Y8ZgE06gASPRTD4rNmr0.svg"}
+              />
+              <Text8>+03213545613</Text8>
+              <Fluentballoonfilled
+                src={"https://file.rendit.io/n/srpYxRvN4y9NZrK39FWn.svg"}
+              />
+              <Text9>ldclsd76@gmail.com</Text9>
+            </FlexRow5>
+          </WhiteFlexColumn1>
+          <WhiteFlexRow>
+            <FlexColumn3>
+              <Text10>Activity</Text10>
+              <FlexColumn4>
+                <FlexRow1 alignSelf={"stretch"}>
+                  <Image4
+                    src={"https://file.rendit.io/n/44El41uigOXqKYJTHJbx.svg"}
+                  />
+                  <Text11>Question paper</Text11>
+                </FlexRow1>
+                <Text12 margin={"0px 0px 0px 78px"}>45</Text12>
+              </FlexColumn4>
+            </FlexColumn3>
+            <FlexColumn5 margin={"0px 184px 0px 0px"}>
+              <FlexRow1 alignSelf={"stretch"}>
+                <SilverSquare />
+                <LACAUS textTransform={"capitalize"}>Articles</LACAUS>
+              </FlexRow1>
+              <Text12 margin={"0px 0px 0px 32px"}>60</Text12>
+            </FlexColumn5>
+            <FlexColumn5 margin={"0px 183px 0px 0px"}>
+              <FlexRow8>
+                <Image4
+                  src={"https://file.rendit.io/n/QlgUtKplqJwXlvTuAILN.svg"}
+                />
+                <Text15>Questions</Text15>
+              </FlexRow8>
+              <Text16>264</Text16>
+            </FlexColumn5>
+            <FlexColumn7>
+              <FlexRow1 alignSelf={"stretch"}>
+                <SilverSquare />
+                <LACAUS textTransform={"capitalize"}>Referrals</LACAUS>
+              </FlexRow1>
+              <Text18>02</Text18>
+            </FlexColumn7>
+          </WhiteFlexRow>
+          <Element11>
+            <WhiteFlexColumn2>
+              <FlexRow4 margin={"0"}>
+                <Text10>Work</Text10>
+                <Edit
+                  src={"https://file.rendit.io/n/I4UwwjeoZA9C7vGVGYLC.svg"}
+                />
+              </FlexRow4>
+              <FlexColumn8>
+                <FlexRow11>
+                  <FlexRow12>
+                    <Makisuitcase
+                      src={"https://file.rendit.io/n/VahGbLTnT5nTZeiIdfzy.svg"}
+                    />
+                    <Paragraph1>
+                      Grade 8 Maths at JHGFD Middle School, US{" "}
+                    </Paragraph1>
+                  </FlexRow12>
+                  <Text20>2019 - Current</Text20>
+                </FlexRow11>
+                <Paragraph2>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Scelerisque quis vel, sed aliquam eu orci, consequat, id.
+                  dolor sit amet, consectetur adipiscing elit. Scelerisque quis
+                  vel, sedt.
+                </Paragraph2>
+              </FlexColumn8>
+            </WhiteFlexColumn2>
+            <Element12>
+              <FlexRow13>
+                <Makisuitcase src={""} />
+                <Paragraph1>
+                  Grade 6 Maths at JHGFD Middle School, US{" "}
+                </Paragraph1>
+              </FlexRow13>
+              <Paragraph4>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Scelerisque quis vel, sed aliquam eu orci, consequat, id. dolor
+                sit amet, consectetur adipiscing elit. Scelerisque quis vel,
+                sedt.
+              </Paragraph4>
+              <Text21>2016 - 2019</Text21>
+            </Element12>
+          </Element11>
+        </FlexColumn2>
+      </FlexColumn>
+    </DesktopRoot>
+  );
+};
+const DesktopRoot = styled.div`
+  background-color: #ffffff;
+  display: flex;
+  overflow: hidden;
+  flex-direction: row;
+  justify-content: flex-start;
+  gap: 49px;
+  margin: auto;
+  min-width: 1438px;
+  align-items: flex-start;
+  padding: 1px 1px 0px 1px;
+`;
+const WhiteFlexColumn = styled.div`
+  border-width: 1px;
+  border-color: rgba(22, 22, 22, 0.1);
+  border-style: solid;
+  box-shadow: 0px 8px 24px 0px rgba(22, 22, 22, 0.04);
+  width: 60px;
+  height: 964px;
+  background-color: #ffffff;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 29px;
+`;
+const LogoAsset = styled.img`
+  width: 56px;
+  height: 56px;
+  margin: 0px 0px 79px 0px;
+`;
+const Epmenu = styled.img`
+  width: 24px;
+  height: 24px;
+  align-self: flex-start;
+  margin: 0px 0px 35px 11px;
+`;
+const Mdiaccount = styled.img`
+  width: 24px;
+  height: 24px;
+  margin: 0px 0px 342px 0px;
+`;
+const Cilaccountlogout = styled.img`
+  width: 24px;
+  height: 24px;
+`;
+const FlexColumn = styled.div`
+  align-self: stretch;
+  width: 1244px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  flex-grow: 1;
+  align-items: flex-start;
+`;
+const FlexRow = styled.div`
+  width: 472px;
+  display: flex;
+  align-self: flex-end;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  margin: 0px 0px 26px 0px;
+`;
+const NeonCarrotFlexRow = styled.div`
+  backdrop-filter: blur(9px);
+  background-color: rgba(247, 148, 29, 0.5);
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  border-radius: 8px;
+  padding: 8px 15px 8px 19px;
+  margin: 0px 32px 0px 0px;
+`;
+const Element8 = styled.div`
+  width: 134px;
+  height: 32px;
+  position: relative;
+`;
+const Text1 = styled.div`
+  width: 108px;
+  height: 32px;
+  display: flex;
+  font-size: 16px;
+  font-family: Open Sans;
+  font-weight: 400;
+  text-transform: capitalize;
+  color: rgba(22, 22, 22, 0.8);
+  position: absolute;
+  flex-direction: column;
+  align-items: start;
+  justify-content: center;
+`;
+const Text2 = styled.div`
+  text-align: right;
+  width: 30.5px;
+  height: 32px;
+  display: flex;
+  font-size: 16px;
+  font-family: Open Sans;
+  font-weight: 700;
+  text-transform: capitalize;
+  color: rgba(22, 22, 22, 0.8);
+  position: absolute;
+  left: 103.18px;
+  flex-direction: column;
+  align-items: end;
+  justify-content: center;
+`;
+const UnsplashWFsNyI1 = styled.img`
+  width: 36px;
+  height: 36px;
+`;
+const Text3 = styled.div`
+  width: 132px;
+  height: 24px;
+  display: flex;
+  font-size: 18px;
+  font-family: Open Sans;
+  font-weight: 400;
+  text-transform: capitalize;
+  color: rgba(22, 22, 22, 0.8);
+  flex-direction: column;
+  align-items: start;
+  justify-content: center;
+`;
+const FlexRow2 = styled.div`
+  align-self: stretch;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  margin: 0px 0px 48px 0px;
+`;
+const UnsplashWFsNyI = styled.img`
+  width: 120px;
+  height: 120px;
+  margin: 0px 36px 0px 0px;
+`;
+const FlexColumn1 = styled.div`
+  width: 187px;
+  display: flex;
+  flex-direction: column;
+  gap: 21px;
+  flex-grow: 1;
+  align-items: center;
+  padding: 13px 0px 9px 0px;
+  margin: 0px 569px 0px 0px;
+`;
+const Text4 = styled.div`
+  font-size: 32px;
+  font-family: Open Sans;
+  font-weight: 400;
+  text-transform: capitalize;
+  color: #161616;
+`;
+const FlexRow3 = styled.div`
+  display: flex;
+  align-self: flex-start;
+  flex-direction: row;
+  gap: 9px;
+  justify-content: center;
+  align-items: center;
+`;
+const Room = styled.img`
+  width: 32px;
+  height: 32px;
+  align-self: flex-start;
+`;
+const Element9 = styled.div`
+  width: 248px;
+  height: 64px;
+  position: relative;
+  margin: 0px 16px 0px 0px;
+`;
+const Element10 = styled.div`
+  border-width: 2px;
+  border-color: #14a9ff;
+  border-style: solid;
+  width: 244px;
+  height: 60px;
+  position: absolute;
+  top: -2px;
+  left: -2px;
+  border-radius: 8px;
+`;
+const Text5 = styled.div`
+  text-align: center;
+  width: 248px;
+  height: 64px;
+  display: flex;
+  font-size: 20px;
+  font-family: Open Sans;
+  font-weight: 400;
+  color: #14a9ff;
+  position: absolute;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+const Morehoriz = styled.img`
+  width: 48px;
+  height: 48px;
+`;
+const FlexColumn2 = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  gap: 34px;
+  align-items: center;
+  padding: 0px 1px;
+`;
+const WhiteFlexColumn1 = styled.div`
+  border-width: 1px;
+  border-color: rgba(22, 22, 22, 0.1);
+  border-style: solid;
+  box-shadow: 0px 4px 48px 0px rgba(0, 0, 0, 0.04);
+  width: 1180px;
+  height: 200px;
+  background-color: #ffffff;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  border-radius: 16px;
+  padding: 21px;
+`;
+const Text6 = styled.div`
+  width: 122px;
+  height: 32px;
+  display: flex;
+  font-size: 32px;
+  font-family: Open Sans;
+  font-weight: 400;
+  color: #161616;
+  flex-direction: column;
+  align-items: start;
+  justify-content: center;
+`;
+const Paragraph = styled.div`
+  width: 1176px;
+  font-size: 24px;
+  font-family: Open Sans;
+  font-weight: 400;
+  color: #aaaaaa;
+  align-self: center;
+  margin: 0px 0px 54px 0px;
+`;
+const FlexRow5 = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+`;
+const Text7 = styled.div`
+  width: 128px;
+  height: 32px;
+  display: flex;
+  font-size: 24px;
+  font-family: Open Sans;
+  font-weight: 400;
+  color: rgba(22, 22, 22, 0.8);
+  flex-direction: column;
+  align-items: start;
+  justify-content: center;
+  margin: 0px 48px 0px 0px;
+`;
+const Text8 = styled.div`
+  width: 170px;
+  height: 32px;
+  display: flex;
+  font-size: 24px;
+  font-family: Open Sans;
+  font-weight: 400;
+  color: rgba(22, 22, 22, 0.8);
+  flex-direction: column;
+  align-items: start;
+  justify-content: center;
+  margin: 0px 42px 0px 0px;
+`;
+const Text9 = styled.div`
+  width: 230px;
+  height: 32px;
+  display: flex;
+  font-size: 24px;
+  font-family: Open Sans;
+  font-weight: 400;
+  color: rgba(22, 22, 22, 0.8);
+  flex-direction: column;
+  align-items: start;
+  justify-content: center;
+`;
+const WhiteFlexRow = styled.div`
+  border-width: 1px;
+  border-color: rgba(22, 22, 22, 0.1);
+  border-style: solid;
+  box-shadow: 0px 4px 48px 0px rgba(0, 0, 0, 0.04);
+  width: 1180px;
+  height: 155px;
+  background-color: #ffffff;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: flex-end;
+  border-radius: 16px;
+  padding: 21px 21px 28px 21px;
+`;
+const FlexColumn3 = styled.div`
+  align-self: stretch;
+  width: 248px;
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
+  justify-content: center;
+  flex-grow: 1;
+  align-items: flex-start;
+  margin: 0px 158px 0px 0px;
+`;
+const FlexColumn4 = styled.div`
+  display: flex;
+  align-self: flex-end;
+  flex-direction: column;
+  gap: 22px;
+  justify-content: center;
+  align-items: flex-start;
+`;
+const Text11 = styled.div`
+  width: 192px;
+  height: 32px;
+  display: flex;
+  font-size: 24px;
+  font-family: Open Sans;
+  font-weight: 400;
+  text-transform: capitalize;
+  color: #aaaaaa;
+  flex-direction: column;
+  align-items: start;
+  justify-content: center;
+`;
+const FlexRow8 = styled.div`
+  align-self: stretch;
+  display: flex;
+  flex-direction: row;
+  gap: 1px;
+  justify-content: center;
+  align-items: center;
+`;
+const Text15 = styled.div`
+  text-align: center;
+  font-size: 24px;
+  font-family: Open Sans;
+  font-weight: 400;
+  text-transform: capitalize;
+  color: #aaaaaa;
+`;
+const Text16 = styled.div`
+  text-align: center;
+  width: 60px;
+  font-size: 24px;
+  font-family: Open Sans;
+  font-weight: 400;
+  text-transform: capitalize;
+  color: rgba(22, 22, 22, 0.8);
+  margin: 0px 0px 0px 36px;
+`;
+const FlexColumn7 = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 13px;
+  justify-content: center;
+  align-items: flex-start;
+  margin: 0px 0px 8px 0px;
+`;
+const Text18 = styled.div`
+  text-align: center;
+  width: 40px;
+  font-size: 24px;
+  font-family: Open Sans;
+  font-weight: 400;
+  text-transform: capitalize;
+  color: rgba(22, 22, 22, 0.8);
+  margin: 0px 0px 0px 40px;
+`;
+const Element11 = styled.div`
+  height: 219px;
+  position: relative;
+  min-width: 1222px;
+`;
+const WhiteFlexColumn2 = styled.div`
+  border-width: 1px;
+  border-color: rgba(22, 22, 22, 0.1);
+  border-style: solid;
+  box-shadow: 0px 4px 48px 0px rgba(0, 0, 0, 0.04);
+  width: 1180px;
+  height: 167px;
+  background-color: #ffffff;
+  display: flex;
+  position: absolute;
+  flex-direction: column;
+  gap: 32px;
+  align-items: center;
+  border-radius: 16px;
+  padding: 21px 21px 31px 21px;
+`;
+const FlexColumn8 = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 6px;
+  justify-content: center;
+  align-items: flex-end;
+`;
+const FlexRow11 = styled.div`
+  align-self: stretch;
+  height: 39px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: flex-start;
+`;
+const FlexRow12 = styled.div`
+  display: flex;
+  align-self: flex-end;
+  flex-direction: row;
+  gap: 16px;
+  justify-content: center;
+  align-items: center;
+`;
+const Text20 = styled.div`
+  text-align: right;
+  width: 214px;
+  height: 34px;
+  display: flex;
+  font-size: 24px;
+  font-family: Open Sans;
+  font-weight: 400;
+  color: rgba(22, 22, 22, 0.8);
+  flex-direction: column;
+  align-items: end;
+  justify-content: center;
+`;
+const Paragraph2 = styled.div`
+  width: 1136px;
+  font-size: 20px;
+  font-family: Open Sans;
+  font-weight: 400;
+  color: #aaaaaa;
+`;
+const Element12 = styled.div`
+  width: 1176px;
+  height: 4px;
+  position: absolute;
+  top: 215px;
+  left: 23px;
+`;
+const FlexRow13 = styled.div`
+  display: flex;
+  position: absolute;
+  top: 6px;
+  flex-direction: row;
+  gap: 16px;
+  justify-content: center;
+  align-items: center;
+`;
+const Paragraph4 = styled.div`
+  width: 1136px;
+  font-size: 18px;
+  font-family: Open Sans;
+  font-weight: 400;
+  color: #aaaaaa;
+  position: absolute;
+  top: 45px;
+  left: 40px;
+`;
+const Text21 = styled.div`
+  text-align: right;
+  width: 214px;
+  height: 4px;
+  display: flex;
+  font-size: 24px;
+  font-family: Open Sans;
+  font-weight: 400;
+  color: rgba(22, 22, 22, 0.8);
+  position: absolute;
+  left: 962px;
+  flex-direction: column;
+  align-items: end;
+  justify-content: center;
+`;
+const Fluenthomefilled = styled.img`
+  width: 24px;
+  height: 24px;
+  margin: 0px 0px 48px 0px;
+`;
+const Search = styled.img`
+  width: 24px;
+  height: 24px;
+  margin: 0px 24px 0px 0px;
+`;
+const FlexRow1 = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 8px;
+  justify-content: center;
+  align-items: center;
+  align-self: ${(props) => props.alignSelf};
+`;
+const LACAUS = styled.div`
+  font-size: 24px;
+  font-family: Open Sans;
+  font-weight: 400;
+  color: #aaaaaa;
+  white-space: pre-wrap;
+  text-transform: ${(props) => props.textTransform};
+`;
+const FlexRow4 = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin: ${(props) => props.margin};
+`;
+const Edit = styled.img`
+  width: 32px;
+  height: 32px;
+`;
+const Fluentballoonfilled = styled.img`
+  width: 32px;
+  height: 32px;
+  margin: 0px 8px 0px 0px;
+`;
+const Text10 = styled.div`
+  width: 107px;
+  height: 32px;
+  display: flex;
+  font-size: 32px;
+  font-family: Open Sans;
+  font-weight: 400;
+  flex-direction: column;
+  align-items: start;
+  justify-content: center;
+`;
+const Image4 = styled.img`
+  width: 24px;
+  height: 24px;
+  align-self: flex-start;
+  margin: 3px 0px 0px 0px;
+`;
+const Text12 = styled.div`
+  text-align: center;
+  width: 42px;
+  font-size: 24px;
+  font-family: Open Sans;
+  font-weight: 400;
+  text-transform: capitalize;
+  color: rgba(22, 22, 22, 0.8);
+  margin: ${(props) => props.margin};
+`;
+const FlexColumn5 = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 21px;
+  justify-content: center;
+  align-items: flex-start;
+  margin: ${(props) => props.margin};
+`;
+const SilverSquare = styled.div`
+  width: 24px;
+  height: 24px;
+  background-color: #c4c4c4;
+  align-self: flex-start;
+  margin: 3px 0px 0px 0px;
+`;
+const Makisuitcase = styled.img`
+  width: 24px;
+  height: 24px;
+  align-self: flex-start;
+  margin: 4px 0px 0px 0px;
+`;
+const Paragraph1 = styled.div`
+  font-size: 24px;
+  font-family: Open Sans;
+  font-weight: 400;
+  color: #161616;
+  white-space: pre-wrap;
+`;
